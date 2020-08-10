11709d80f78bb41d66681069572f87404bf3bd1d
ningenMe <amohkuru@gmail.com>
2020-08-10 11:59:16 +0900

    solved

---
+++ atcoder/Other/discovery2016-qual/C.solve.cpp
@@ -0,0 +1,230 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define ALL(obj) (obj).begin(),(obj).end()
+#define SPEED cin.tie(0);ios::sync_with_stdio(false);
+
+template<class T> using PQ = priority_queue<T>;
+template<class T> using PQR = priority_queue<T,vector<T>,greater<T>>;
+
+constexpr long long MOD = (long long)1e9 + 7;
+constexpr long long MOD2 = 998244353;
+constexpr long long HIGHINF = (long long)1e18;
+constexpr long long LOWINF = (long long)1e15;
+constexpr long double PI = 3.1415926535897932384626433L;
+
+template <class T> vector<T> multivector(size_t N,T init){return vector<T>(N,init);}
+template <class... T> auto multivector(size_t N,T... t){return vector<decltype(multivector(t...))>(N,multivector(t...));}
+template <class T> void corner(bool flg, T hoge) {if (flg) {cout << hoge << endl; exit(0);}}
+template <class T, class U>ostream &operator<<(ostream &o, const map<T, U>&obj) {o << "{"; for (auto &x : obj) o << " {" << x.first << " : " << x.second << "}" << ","; o << " }"; return o;}
+template <class T>ostream &operator<<(ostream &o, const set<T>&obj) {o << "{"; for (auto itr = obj.begin(); itr != obj.end(); ++itr) o << (itr != obj.begin() ? ", " : "") << *itr; o << "}"; return o;}
+template <class T>ostream &operator<<(ostream &o, const multiset<T>&obj) {o << "{"; for (auto itr = obj.begin(); itr != obj.end(); ++itr) o << (itr != obj.begin() ? ", " : "") << *itr; o << "}"; return o;}
+template <class T>ostream &operator<<(ostream &o, const vector<T>&obj) {o << "{"; for (int i = 0; i < (int)obj.size(); ++i)o << (i > 0 ? ", " : "") << obj[i]; o << "}"; return o;}
+template <class T, class U>ostream &operator<<(ostream &o, const pair<T, U>&obj) {o << "{" << obj.first << ", " << obj.second << "}"; return o;}
+void print(void) {cout << endl;}
+template <class Head> void print(Head&& head) {cout << head;print();}
+template <class Head, class... Tail> void print(Head&& head, Tail&&... tail) {cout << head << " ";print(forward<Tail>(tail)...);}
+template <class T> void chmax(T& a, const T b){a=max(a,b);}
+template <class T> void chmin(T& a, const T b){a=min(a,b);}
+std::vector<std::string> split(const std::string &str, const char delemiter) {std::vector<std::string> res;std::stringstream ss(str);std::string buffer; while( std::getline(ss, buffer, delemiter) ) res.push_back(buffer); return res;}
+void YN(bool flg) {cout << (flg ? "YES" : "NO") << endl;}
+void Yn(bool flg) {cout << (flg ? "Yes" : "No") << endl;}
+void yn(bool flg) {cout << (flg ? "yes" : "no") << endl;}
+
+/*
+ * @title ModInt
+ */
+template<long long mod> class ModInt {
+public:
+	long long x;
+	constexpr ModInt():x(0) {
+		// do nothing
+	}
+	constexpr ModInt(long long y) : x(y>=0?(y%mod): (mod - (-y)%mod)%mod) {
+		// do nothing
+	}
+	ModInt &operator+=(const ModInt &p) {
+		if((x += p.x) >= mod) x -= mod;
+		return *this;
+	}
+	ModInt &operator+=(const long long y) {
+		ModInt p(y);
+		if((x += p.x) >= mod) x -= mod;
+		return *this;
+	}
+	ModInt &operator+=(const int y) {
+		ModInt p(y);
+		if((x += p.x) >= mod) x -= mod;
+		return *this;
+	}
+	ModInt &operator-=(const ModInt &p) {
+		if((x += mod - p.x) >= mod) x -= mod;
+		return *this;
+	}
+	ModInt &operator-=(const long long y) {
+		ModInt p(y);
+		if((x += mod - p.x) >= mod) x -= mod;
+		return *this;
+	}
+	ModInt &operator-=(const int y) {
+		ModInt p(y);
+		if((x += mod - p.x) >= mod) x -= mod;
+		return *this;
+	}
+	ModInt &operator*=(const ModInt &p) {
+		x = (x * p.x % mod);
+		return *this;
+	}
+	ModInt &operator*=(const long long y) {
+		ModInt p(y);
+		x = (x * p.x % mod);
+		return *this;
+	}
+	ModInt &operator*=(const int y) {
+		ModInt p(y);
+		x = (x * p.x % mod);
+		return *this;
+	}
+	ModInt &operator^=(const ModInt &p) {
+		x = (x ^ p.x) % mod;
+		return *this;
+	}
+	ModInt &operator^=(const long long y) {
+		ModInt p(y);
+		x = (x ^ p.x) % mod;
+		return *this;
+	}
+	ModInt &operator^=(const int y) {
+		ModInt p(y);
+		x = (x ^ p.x) % mod;
+		return *this;
+	}
+	ModInt &operator/=(const ModInt &p) {
+		*this *= p.inv();
+		return *this;
+	}
+	ModInt &operator/=(const long long y) {
+		ModInt p(y);
+		*this *= p.inv();
+		return *this;
+	}
+	ModInt &operator/=(const int y) {
+		ModInt p(y);
+		*this *= p.inv();
+		return *this;
+	}
+	ModInt operator=(const int y) {
+		ModInt p(y);
+		*this = p;
+		return *this;
+	}
+	ModInt operator=(const long long y) {
+		ModInt p(y);
+		*this = p;
+		return *this;
+	}
+	ModInt operator-() const { return ModInt(-x); }
+	ModInt operator++() { 
+		x++;
+		if(x>=mod) x-=mod;
+		return *this; 
+	}
+	ModInt operator--() { 
+		x--;
+		if(x<0) x+=mod;
+		return *this; 
+	}
+	ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+	ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+	ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+	ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+	ModInt operator^(const ModInt &p) const { return ModInt(*this) ^= p; }
+	bool operator==(const ModInt &p) const { return x == p.x; }
+	bool operator!=(const ModInt &p) const { return x != p.x; }
+	ModInt inv() const {
+		int a = x, b = mod, u = 1, v = 0, t;
+		while(b > 0) {
+			t = a / b;
+			swap(a -= t * b, b);
+			swap(u -= t * v, v);
+		}
+		return ModInt(u);
+	}
+	ModInt pow(long long n) const {
+		ModInt ret(1), mul(x);
+		while(n > 0) {
+			if(n & 1) ret *= mul;
+			mul *= mul;
+			n >>= 1;
+		}
+		return ret;
+	}
+	friend ostream &operator<<(ostream &os, const ModInt &p) {
+		return os << p.x;
+	}
+	friend istream &operator>>(istream &is, ModInt &a) {
+		long long t;
+		is >> t;
+		a = ModInt<mod>(t);
+		return (is);
+	}
+};
+using modint = ModInt<MOD>;
+
+string S;
+int N,K;
+vector<int> edge;
+
+//[l,r]
+//O(N)
+vector<modint> rec(int l, int r) {
+    int m=r-l+1;
+    vector<modint> dp(2*m+1,0);
+    // O(1)
+    if(l+1==r) {
+        if(K>=2) dp[m+2]=dp[m-2]=1;
+        dp[m]=2;
+        return dp;
+    }
+    int c = edge[r];
+    // O(N^2)
+    if(l!=c) {
+        auto ldp=rec(l,c-1),rdp=rec(c,r);
+        int ml=c-l,mr=r-c+1;
+        for(int i=-ml;i<=ml;++i)for(int j=-mr;j<=mr;++j) if(0<=i+j+m&&i+j+m<=2*N) dp[i+j+m] += ldp[i+ml]*rdp[j+mr];
+        return dp;
+    }
+    // O(N)
+    else{
+        auto rdp=rec(l+1,r-1);
+        int ml=2,mr=r-(l+1);
+        vector<modint> ldp(2*ml+1);
+        if(K>=2) ldp[ml-2]=ldp[ml+2]=1;
+        ldp[ml]=2;
+        for(int i=-ml;i<=ml;++i)for(int j=-mr;j<=mr;++j) if(0<=i+j+m&&i+j+m<=2*m&&m-K<=i+j+m&&i+j+m<=m+K) dp[i+j+m] += ldp[i+ml]*rdp[j+mr];
+        return dp;
+    }
+}
+
+int main() {
+    cin >> S;
+    N = S.size();
+    cin >> K;
+    edge.resize(N,-1);
+    stack<int> st;
+    for(int i = 0; i < N; ++i) {
+        if(S[i]=='(') {
+            st.push(i);
+        }
+        else{
+            edge[i]=st.top();
+            st.pop();
+        }
+    }
+    auto dp=rec(0,N-1);
+    modint ans=0;
+    for(int i = 0; i <= 2*N; ++i) ans += dp[i];
+    cout << ans << endl;
+    return 0;
+}
