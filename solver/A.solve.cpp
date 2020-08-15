e501fda20346fc25628e08dfb8270c50920f1f93
kuru <amohkuru@gmail.com>
2020-08-15 13:36:49 +0900

    solved

--- solver/A.solve.cpp
+++ solver/A.solve.cpp
@@ -3,12 +3,9 @@ using namespace std;
 using ll = long long;
 
 #define ALL(obj) (obj).begin(),(obj).end()
-#define SPEED cin.tie(0);ios::sync_with_stdio(false);
+template<class T> using priority_queue_reverse = priority_queue<T,vector<T>,greater<T>>;
 
-template<class T> using PQ = priority_queue<T>;
-template<class T> using PQR = priority_queue<T,vector<T>,greater<T>>;
-
-constexpr long long MOD = (long long)1e9 + 7;
+constexpr long long MOD = 1'000'000'000LL + 7;
 constexpr long long MOD2 = 998244353;
 constexpr long long HIGHINF = (long long)1e18;
 constexpr long long LOWINF = (long long)1e15;
@@ -27,11 +24,13 @@ template <class Head> void print(Head&& head) {cout << head;print();}
 template <class Head, class... Tail> void print(Head&& head, Tail&&... tail) {cout << head << " ";print(forward<Tail>(tail)...);}
 template <class T> void chmax(T& a, const T b){a=max(a,b);}
 template <class T> void chmin(T& a, const T b){a=min(a,b);}
-std::vector<std::string> split(const std::string &str, const char delemiter) {std::vector<std::string> res;std::stringstream ss(str);std::string buffer; while( std::getline(ss, buffer, delemiter) ) res.push_back(buffer); return res;}
+vector<string> split(const string &str, const char delemiter) {vector<string> res;stringstream ss(str);string buffer; while( getline(ss, buffer, delemiter) ) res.push_back(buffer); return res;}
+int msb(int x) {return x?31-__builtin_clz(x):-1;}
 void YN(bool flg) {cout << (flg ? "YES" : "NO") << endl;}
 void Yn(bool flg) {cout << (flg ? "Yes" : "No") << endl;}
 void yn(bool flg) {cout << (flg ? "yes" : "no") << endl;}
 
 int main() {
+    cin.tie(0);ios::sync_with_stdio(false);
     return 0;
 }
