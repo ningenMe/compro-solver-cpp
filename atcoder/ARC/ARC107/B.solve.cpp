26df54cf6ff73b4bfc91f92e0252f74a117c78b0
ningenMe <amohkuru@gmail.com>
2020-11-17 09:42:53 +0900

    [add] solved

---
+++ atcoder/ARC/ARC107/B.solve.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define ALL(obj) (obj).begin(),(obj).end()
+template<class T> using priority_queue_reverse = priority_queue<T,vector<T>,greater<T>>;
+
+constexpr long long MOD = 1'000'000'000LL + 7; //'
+constexpr long long MOD2 = 998244353;
+constexpr long long HIGHINF = (long long)1e18;
+constexpr long long LOWINF = (long long)1e15;
+constexpr long double PI = 3.1415926535897932384626433L;
+
+template <class T> vector<T> multivector(size_t N,T init){return vector<T>(N,init);}
+template <class... T> auto multivector(size_t N,T... t){return vector<decltype(multivector(t...))>(N,multivector(t...));}
+template <class T> void corner(bool flg, T hoge) {if (flg) {cout << hoge << endl; exit(0);}}
+template <class T, class U>ostream &operator<<(ostream &o, const map<T, U>&obj) {o << "{"; for (auto &x : obj) o << " {" << x.first << " : " << x.second << "}" << ","; o << " }"; return o;}
+template <class T>ostream &operator<<(ostream &o, const set<T>&obj) {o << "{"; for (auto itr = obj.begin(); itr != obj.end(); ++itr) o << (itr != obj.begin() ? ", " : "") << *itr; o << "}"; return o;}
+template <class T>ostream &operator<<(ostream &o, const multiset<T>&obj) {o << "{"; for (auto itr = obj.begin(); itr != obj.end(); ++itr) o << (itr != obj.begin() ? ", " : "") << *itr; o << "}"; return o;}
+template <class T>ostream &operator<<(ostream &o, const vector<T>&obj) {o << "{"; for (int i = 0; i < (int)obj.size(); ++i)o << (i > 0 ? ", " : "") << obj[i]; o << "}"; return o;}
+template <class T, class U>ostream &operator<<(ostream &o, const pair<T, U>&obj) {o << "{" << obj.first << ", " << obj.second << "}"; return o;}
+void print(void) {cout << endl;}
+template <class Head> void print(Head&& head) {cout << head;print();}
+template <class Head, class... Tail> void print(Head&& head, Tail&&... tail) {cout << head << " ";print(forward<Tail>(tail)...);}
+template <class T> void chmax(T& a, const T b){a=max(a,b);}
+template <class T> void chmin(T& a, const T b){a=min(a,b);}
+vector<string> split(const string &str, const char delemiter) {vector<string> res;stringstream ss(str);string buffer; while( getline(ss, buffer, delemiter) ) res.push_back(buffer); return res;}
+int msb(int x) {return x?31-__builtin_clz(x):-1;}
+void YN(bool flg) {cout << (flg ? "YES" : "NO") << endl;}
+void Yn(bool flg) {cout << (flg ? "Yes" : "No") << endl;}
+void yn(bool flg) {cout << (flg ? "yes" : "no") << endl;}
+
+/**
+ * @url https://atcoder.jp/contests/arc107/tasks/arc107_b
+ * @est 300
+ */ 
+int main() {
+    cin.tie(0);ios::sync_with_stdio(false);
+
+    ll N,K; cin >> N >> K;
+    //e = a+b
+    //f = c+d
+    ll ans = 0;
+    for(ll e=2; e<=2*N;++e) {
+        ll f = e - K;
+        if(f < 2 || 2*N < f) continue;
+        ll l=0,r=0;
+        if(e > N+1) {
+            // N+1,N+2,......,e-1より
+            ll t = (e-1) - N;
+            l = e-1 - 2*t;
+        }
+        else {
+            l += e-1;
+        }
+
+        if(f > N+1) {
+            // N+1,N+2,......,e-1より
+            ll t = (f-1) - N;
+            r = f-1 - 2*t;
+        }
+        else {
+            r += f-1;
+        }
+
+        ans += l*r;
+    }
+    cout << ans << endl;
+    return 0;
+}
---
+++ atcoder/ARC/ARC107/C.solve.cpp
@@ -0,0 +1,193 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#define ALL(obj) (obj).begin(),(obj).end()
+template<class T> using priority_queue_reverse = priority_queue<T,vector<T>,greater<T>>;
+
+constexpr long long MOD = 1'000'000'000LL + 7; //'
+constexpr long long MOD2 = 998244353;
+constexpr long long HIGHINF = (long long)1e18;
+constexpr long long LOWINF = (long long)1e15;
+constexpr long double PI = 3.1415926535897932384626433L;
+
+template <class T> vector<T> multivector(size_t N,T init){return vector<T>(N,init);}
+template <class... T> auto multivector(size_t N,T... t){return vector<decltype(multivector(t...))>(N,multivector(t...));}
+template <class T> void corner(bool flg, T hoge) {if (flg) {cout << hoge << endl; exit(0);}}
+template <class T, class U>ostream &operator<<(ostream &o, const map<T, U>&obj) {o << "{"; for (auto &x : obj) o << " {" << x.first << " : " << x.second << "}" << ","; o << " }"; return o;}
+template <class T>ostream &operator<<(ostream &o, const set<T>&obj) {o << "{"; for (auto itr = obj.begin(); itr != obj.end(); ++itr) o << (itr != obj.begin() ? ", " : "") << *itr; o << "}"; return o;}
+template <class T>ostream &operator<<(ostream &o, const multiset<T>&obj) {o << "{"; for (auto itr = obj.begin(); itr != obj.end(); ++itr) o << (itr != obj.begin() ? ", " : "") << *itr; o << "}"; return o;}
+template <class T>ostream &operator<<(ostream &o, const vector<T>&obj) {o << "{"; for (int i = 0; i < (int)obj.size(); ++i)o << (i > 0 ? ", " : "") << obj[i]; o << "}"; return o;}
+template <class T, class U>ostream &operator<<(ostream &o, const pair<T, U>&obj) {o << "{" << obj.first << ", " << obj.second << "}"; return o;}
+void print(void) {cout << endl;}
+template <class Head> void print(Head&& head) {cout << head;print();}
+template <class Head, class... Tail> void print(Head&& head, Tail&&... tail) {cout << head << " ";print(forward<Tail>(tail)...);}
+template <class T> void chmax(T& a, const T b){a=max(a,b);}
+template <class T> void chmin(T& a, const T b){a=min(a,b);}
+vector<string> split(const string &str, const char delemiter) {vector<string> res;stringstream ss(str);string buffer; while( getline(ss, buffer, delemiter) ) res.push_back(buffer); return res;}
+int msb(int x) {return x?31-__builtin_clz(x):-1;}
+void YN(bool flg) {cout << (flg ? "YES" : "NO") << endl;}
+void Yn(bool flg) {cout << (flg ? "Yes" : "No") << endl;}
+void yn(bool flg) {cout << (flg ? "yes" : "no") << endl;}
+
+/*
+ * @title ModInt
+ * @docs md/util/ModInt.md
+ */
+template<long long mod> class ModInt {
+public:
+    long long x;
+    constexpr ModInt():x(0) {}
+    constexpr ModInt(long long y) : x(y>=0?(y%mod): (mod - (-y)%mod)%mod) {}
+    ModInt &operator+=(const ModInt &p) {if((x += p.x) >= mod) x -= mod;return *this;}
+    ModInt &operator+=(const long long y) {ModInt p(y);if((x += p.x) >= mod) x -= mod;return *this;}
+    ModInt &operator+=(const int y) {ModInt p(y);if((x += p.x) >= mod) x -= mod;return *this;}
+    ModInt &operator-=(const ModInt &p) {if((x += mod - p.x) >= mod) x -= mod;return *this;}
+    ModInt &operator-=(const long long y) {ModInt p(y);if((x += mod - p.x) >= mod) x -= mod;return *this;}
+    ModInt &operator-=(const int y) {ModInt p(y);if((x += mod - p.x) >= mod) x -= mod;return *this;}
+    ModInt &operator*=(const ModInt &p) {x = (x * p.x % mod);return *this;}
+    ModInt &operator*=(const long long y) {ModInt p(y);x = (x * p.x % mod);return *this;}
+    ModInt &operator*=(const int y) {ModInt p(y);x = (x * p.x % mod);return *this;}
+    ModInt &operator^=(const ModInt &p) {x = (x ^ p.x) % mod;return *this;}
+    ModInt &operator^=(const long long y) {ModInt p(y);x = (x ^ p.x) % mod;return *this;}
+    ModInt &operator^=(const int y) {ModInt p(y);x = (x ^ p.x) % mod;return *this;}
+    ModInt &operator/=(const ModInt &p) {*this *= p.inv();return *this;}
+    ModInt &operator/=(const long long y) {ModInt p(y);*this *= p.inv();return *this;}
+    ModInt &operator/=(const int y) {ModInt p(y);*this *= p.inv();return *this;}
+    ModInt operator=(const int y) {ModInt p(y);*this = p;return *this;}
+    ModInt operator=(const long long y) {ModInt p(y);*this = p;return *this;}
+    ModInt operator-() const {return ModInt(-x); }
+    ModInt operator++() {x++;if(x>=mod) x-=mod;return *this;}
+    ModInt operator--() {x--;if(x<0) x+=mod;return *this;}
+    ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+    ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+    ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+    ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+    ModInt operator^(const ModInt &p) const { return ModInt(*this) ^= p; }
+    bool operator==(const ModInt &p) const { return x == p.x; }
+    bool operator!=(const ModInt &p) const { return x != p.x; }
+    ModInt inv() const {int a=x,b=mod,u=1,v=0,t;while(b > 0) {t = a / b;swap(a -= t * b, b);swap(u -= t * v, v);} return ModInt(u);}
+    ModInt pow(long long n) const {ModInt ret(1), mul(x);for(;n > 0;mul *= mul,n >>= 1) if(n & 1) ret *= mul;return ret;}
+    friend ostream &operator<<(ostream &os, const ModInt &p) {return os << p.x;}
+    friend istream &operator>>(istream &is, ModInt &a) {long long t;is >> t;a = ModInt<mod>(t);return (is);}
+};
+using modint = ModInt<MOD2>;
+
+/*
+ * @title UnionFindTree - Union Find 木
+ * @docs md/graph/UnionFindTree.md
+ */
+class UnionFindTree {
+	vector<int> parent,maxi,mini;
+	inline int root(int n) {
+		return (parent[n]<0?n:parent[n] = root(parent[n]));
+	}
+public:
+	UnionFindTree(int N = 1) : parent(N,-1),maxi(N),mini(N){
+		iota(maxi.begin(),maxi.end(),0);
+		iota(mini.begin(),mini.end(),0);
+	}
+	inline bool connected(int n, int m) {
+		return root(n) == root(m);
+	}
+	inline void merge(int n, int m) {
+		n = root(n);
+		m = root(m);
+		if (n == m) return;
+		if(parent[n]>parent[m]) swap(n, m);
+		parent[n] += parent[m];
+		parent[m] = n;
+		maxi[n] = std::max(maxi[n],maxi[m]);
+		mini[n] = std::min(mini[n],mini[m]);
+	}
+	inline int min(int n) {
+		return mini[root(n)];
+	}
+	inline int max(int n) {
+		return maxi[root(n)];
+	}
+	inline int size(int n){
+		return (-parent[root(n)]);
+	}
+	inline int operator[](int n) {
+		return root(n);
+	}
+	inline void print() {
+		for(int i = 0; i < parent.size(); ++i) cout << root(i) << " ";
+		cout << endl;
+	}
+};
+
+/*
+ * @title CombinationMod - mod上の二項係数・階乗
+ * @docs md/math/CombinationMod.md
+ */
+template<long long mod> class CombinationMod {
+	vector<long long> fac,finv,inv;
+public:
+	CombinationMod(int N) : fac(N + 1), finv(N + 1), inv(N + 1) {
+		fac[0] = fac[1] = finv[0] = finv[1] = inv[1] = 1;
+		for (int i = 2; i <= N; ++i) {
+			fac[i] = fac[i - 1] * i % mod;
+			inv[i] = mod - inv[mod%i] * (mod / i) % mod;
+			finv[i] = finv[i - 1] * inv[i] % mod;
+		}
+	}
+	inline long long binom(int n, int k) {
+		return ((n < 0 || k < 0 || n < k) ? 0 : fac[n] * (finv[k] * finv[n - k] % mod) % mod);
+	}
+	inline long long factorial(int n) {
+		return fac[n];
+	}
+};
+
+//verify https://atcoder.jp/contests/abc021/tasks/abc021_d
+
+/**
+ * @url https://atcoder.jp/contests/arc107/tasks/arc107_c
+ * @est 400
+ */ 
+int main() {
+    cin.tie(0);ios::sync_with_stdio(false);
+    int N,K; cin >> N >> K;
+    auto A = multivector(N,N,0);
+    for(int i=0;i<N;++i) {
+        for(int j=0;j<N;++j) {
+            cin >> A[i][j];
+        }
+    }
+    CombinationMod<MOD2> CM(N);
+    modint ans=1;
+    {
+        UnionFindTree uf(N);
+        for(int j=0; j < N; ++j) {
+            for(int k=j+1; k<N; ++k) {
+                int flg = 1;
+                for(int i=0;i<N;++i) {
+                    if(A[i][j]+A[i][k]>K) flg = 0;
+                }
+                if(flg) uf.merge(j,k);
+            }
+        }
+        vector<int> cnt(N,0);
+        for(int j=0;j<N;++j) cnt[uf[j]]++;
+        for(int j=0;j<N;++j) ans *= CM.factorial(cnt[j]);
+    }
+    {
+        UnionFindTree uf(N);
+        for(int i=0; i < N; ++i) {
+            for(int k=i+1; k<N; ++k) {
+                int flg = 1;
+                for(int j=0;j<N;++j) {
+                    if(A[i][j]+A[k][j]>K) flg = 0;
+                }
+                if(flg) uf.merge(i,k);
+            }
+        }
+        vector<int> cnt(N,0);
+        for(int i=0;i<N;++i) cnt[uf[i]]++;
+        for(int i=0;i<N;++i) ans *= CM.factorial(cnt[i]);
+    }
+    cout << ans << endl;
+    return 0;
+}
