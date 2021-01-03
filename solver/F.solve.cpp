5e41a94591e65be755f31f9295f113918023a5f1
ningenMe <amohkuru@gmail.com>
2021-01-03 16:28:32 +0900

    [modify] template

--- solver/F.solve.cpp
+++ solver/F.solve.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
+using int128 = __int128_t;
+using int64 = long long;
 
 #define ALL(obj) (obj).begin(),(obj).end()
 template<class T> using priority_queue_reverse = priority_queue<T,vector<T>,greater<T>>;
 
-constexpr long long MOD = 1'000'000'000LL + 7; //'
-constexpr long long MOD2 = 998244353;
-constexpr long long HIGHINF = (long long)1e18;
-constexpr long long LOWINF = (long long)1e15;
+constexpr int64 MOD = 1'000'000'000LL + 7; //'
+constexpr int64 MOD2 = 998244353;
+constexpr int64 HIGHINF = 1'000'000'000'000'000'000LL;
+constexpr int64 LOWINF = 1'000'000'000'000'000LL; //'
 constexpr long double PI = 3.1415926535897932384626433L;
 
 template <class T> vector<T> multivector(size_t N,T init){return vector<T>(N,init);}
@@ -18,6 +19,7 @@ template <class T, class U>ostream &operator<<(ostream &o, const map<T, U>&obj) 
 template <class T>ostream &operator<<(ostream &o, const set<T>&obj) {o << "{"; for (auto itr = obj.begin(); itr != obj.end(); ++itr) o << (itr != obj.begin() ? ", " : "") << *itr; o << "}"; return o;}
 template <class T>ostream &operator<<(ostream &o, const multiset<T>&obj) {o << "{"; for (auto itr = obj.begin(); itr != obj.end(); ++itr) o << (itr != obj.begin() ? ", " : "") << *itr; o << "}"; return o;}
 template <class T>ostream &operator<<(ostream &o, const vector<T>&obj) {o << "{"; for (int i = 0; i < (int)obj.size(); ++i)o << (i > 0 ? ", " : "") << obj[i]; o << "}"; return o;}
+template <class T>ostream &operator<<(ostream &o, const deque<T>&obj) {o << "{"; for (int i = 0; i < (int)obj.size(); ++i)o << (i > 0 ? ", " : "") << obj[i]; o << "}"; return o;}
 template <class T, class U>ostream &operator<<(ostream &o, const pair<T, U>&obj) {o << "{" << obj.first << ", " << obj.second << "}"; return o;}
 void print(void) {cout << endl;}
 template <class Head> void print(Head&& head) {cout << head;print();}
@@ -25,7 +27,8 @@ template <class Head, class... Tail> void print(Head&& head, Tail&&... tail) {co
 template <class T> void chmax(T& a, const T b){a=max(a,b);}
 template <class T> void chmin(T& a, const T b){a=min(a,b);}
 vector<string> split(const string &str, const char delemiter) {vector<string> res;stringstream ss(str);string buffer; while( getline(ss, buffer, delemiter) ) res.push_back(buffer); return res;}
-int msb(int x) {return x?31-__builtin_clz(x):-1;}
+inline constexpr int msb(int x) {return x?31-__builtin_clz(x):-1;}
+inline constexpr int64 ceil_div(const int64 a,const int64 b) {return (a+(b-1))/b;}// return ceil(a/b)
 void YN(bool flg) {cout << (flg ? "YES" : "NO") << endl;}
 void Yn(bool flg) {cout << (flg ? "Yes" : "No") << endl;}
 void yn(bool flg) {cout << (flg ? "yes" : "no") << endl;}
