3bd1f80860895cf2478e94da80ef98ac4b00a8f0
ningenMe <amohkuru@gmail.com>
2022-01-04 02:27:53 +0900

    [add]

---
+++ codeforces/other/Hello2022/D.solve.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+using int128 = __int128_t;
+using int64 = long long;
+
+#define ALL(obj) (obj).begin(),(obj).end()
+template<class T> using priority_queue_reverse = priority_queue<T,vector<T>,greater<T>>;
+
+constexpr int64 MOD = 1'000'000'000LL + 7; //'
+constexpr int64 MOD2 = 998244353;
+constexpr int64 HIGHINF = 1'000'000'000'000'000'000LL;
+constexpr int64 LOWINF = 1'000'000'000'000'000LL; //'
+constexpr long double PI = 3.1415926535897932384626433L;
+
+template <class T> vector<T> multivector(size_t N,T init){return vector<T>(N,init);}
+template <class... T> auto multivector(size_t N,T... t){return vector<decltype(multivector(t...))>(N,multivector(t...));}
+template <class T> void corner(bool flg, T hoge) {if (flg) {cout << hoge << endl; exit(0);}}
+template <class T, class U>ostream &operator<<(ostream &o, const map<T, U>&obj) {o << "{"; for (auto &x : obj) o << " {" << x.first << " : " << x.second << "}" << ","; o << " }"; return o;}
+template <class T>ostream &operator<<(ostream &o, const set<T>&obj) {o << "{"; for (auto itr = obj.begin(); itr != obj.end(); ++itr) o << (itr != obj.begin() ? ", " : "") << *itr; o << "}"; return o;}
+template <class T>ostream &operator<<(ostream &o, const multiset<T>&obj) {o << "{"; for (auto itr = obj.begin(); itr != obj.end(); ++itr) o << (itr != obj.begin() ? ", " : "") << *itr; o << "}"; return o;}
+template <class T>ostream &operator<<(ostream &o, const vector<T>&obj) {o << "{"; for (int i = 0; i < (int)obj.size(); ++i)o << (i > 0 ? ", " : "") << obj[i]; o << "}"; return o;}
+template <class T>ostream &operator<<(ostream &o, const deque<T>&obj) {o << "{"; for (int i = 0; i < (int)obj.size(); ++i)o << (i > 0 ? ", " : "") << obj[i]; o << "}"; return o;}
+template <class T, class U>ostream &operator<<(ostream &o, const pair<T, U>&obj) {o << "{" << obj.first << ", " << obj.second << "}"; return o;}
+void print(void) {cout << endl;}
+template <class Head> void print(Head&& head) {cout << head;print();}
+template <class Head, class... Tail> void print(Head&& head, Tail&&... tail) {cout << head << " ";print(forward<Tail>(tail)...);}
+template <class T> void chmax(T& a, const T b){a=max(a,b);}
+template <class T> void chmin(T& a, const T b){a=min(a,b);}
+vector<string> split(const string &str, const char delemiter) {vector<string> res;stringstream ss(str);string buffer; while( getline(ss, buffer, delemiter) ) res.push_back(buffer); return res;}
+inline constexpr int msb(int x) {return x?31-__builtin_clz(x):-1;}
+inline constexpr int64 ceil_div(const int64 a,const int64 b) {return (a+(b-1))/b;}// return ceil(a/b)
+void YN(bool flg) {cout << (flg ? "YES" : "NO") << endl;}
+void Yn(bool flg) {cout << (flg ? "Yes" : "No") << endl;}
+void yn(bool flg) {cout << (flg ? "yes" : "no") << endl;}
+
+
+void solve() {
+    int N; cin >> N;
+    auto C = multivector(2*N,2*N,0LL);
+    for(int i=0;i<2*N;++i) {
+        for(int j=0;j<2*N;++j) {
+            cin >> C[i][j];
+        }
+    }
+    int64 sum = 0;
+    for(int i=N;i<2*N;++i) {
+        for(int j=N;j<2*N;++j) {
+            sum += C[i][j];
+        }
+    }
+    int64 c = HIGHINF;
+    chmin(c,C[N][0]);
+    chmin(c,C[N][N-1]);
+    chmin(c,C[2*N-1][0]);
+    chmin(c,C[2*N-1][N-1]);
+    chmin(c,C[0][N]);
+    chmin(c,C[0][2*N-1]);
+    chmin(c,C[N-1][N]);
+    chmin(c,C[N-1][2*N-1]);
+    int64 ans = sum + c;
+    cout << ans << "\n";
+}
+
+/**
+ * @url 
+ * @est
+ */ 
+int main() {
+    cin.tie(0);ios::sync_with_stdio(false);
+    int t; cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
