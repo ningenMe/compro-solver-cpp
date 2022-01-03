39c70c9571755c2af3ed0c8f516753548aee6746
ningenMe <amohkuru@gmail.com>
2022-01-03 21:29:48 +0900

    [fix]

--- yukicoder/1100-1200/1170.solve.cpp
+++ yukicoder/1100-1200/1170.solve.cpp
@@ -1,30 +1,72 @@
-#include <bits/stdc++.h>
+#define PROBLEM "https://yukicoder.me/problems/no/1170"
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+#include <numeric>
 using namespace std;
 
-//Union Find Tree
-class UnionFindTree {
-	vector<int> parent,maxi,mini;
-	inline int root(int n) {
-        return (parent[n]<0?n:parent[n] = root(parent[n]));
+/*
+ * @title RangeIndex - 区間index
+ * @docs md/util/RangeIndex.md
+ */
+class RangeIndex{
+	int length;
+public:
+	RangeIndex(const int N) {
+		for (length = 1; length <= N; length *= 2);
 	}
+	//[l,r)
+	vector<int> range(int l,int r) {
+		vector<int> res;
+		for(l += length, r += length; l < r; l >>=1, r >>=1) {
+			if(l&1) res.push_back(l++);
+			if(r&1) res.push_back(--r);
+		}
+		return res;
+	}
+	// ranges that include x
+	vector<int> include_range(int x) {
+		vector<int> res;
+        for(int i=x+length; i; i >>= 1) res.push_back(i);
+		return res;
+	}
+	inline int operator[](int idx) {
+		return idx+length;
+	}
+	inline size_t size(void){
+		return 2*length;
+	}
+};
+
+/*
+ * @title UnionFindTree - Union Find 木
+ * @docs md/graph/UnionFindTree.md
+ */
+class UnionFindTree {
+    vector<int> parent,maxi,mini;
+    inline int root(int n) {
+        return (parent[n]<0?n:parent[n] = root(parent[n]));
+    }
 public:
     UnionFindTree(int N = 1) : parent(N,-1),maxi(N),mini(N){
         iota(maxi.begin(),maxi.end(),0);
         iota(mini.begin(),mini.end(),0);
-	}
+    }
     inline bool connected(int n, int m) {
-		return root(n) == root(m);
-	}
-	inline void merge(int n, int m) {
-		n = root(n);
-		m = root(m);
-		if (n == m) return;
-		if(parent[n]>parent[m]) swap(n, m);
+        return root(n) == root(m);
+    }
+    inline void merge(int n, int m) {
+        n = root(n);
+        m = root(m);
+        if (n == m) return;
+        if(parent[n]>parent[m]) swap(n, m);
         parent[n] += parent[m];
         parent[m] = n;
         maxi[n] = std::max(maxi[n],maxi[m]);
         mini[n] = std::min(mini[n],mini[m]);
-	}
+    }
     inline int min(int n) {
         return mini[root(n)];
     }
@@ -35,34 +77,11 @@ public:
         return (-parent[root(n)]);
     }
     inline int operator[](int n) {
-		return root(n);
-	}
+        return root(n);
+    }
     inline void print() {
         for(int i = 0; i < parent.size(); ++i) cout << root(i) << " ";
         cout << endl;
-    }
-};
-
-class RangeIndex{
-    int length;
-public:
-    RangeIndex(const int N) {
-        for (length = 1; length <= N; length *= 2);
-    }
-    //[l,r)
-    vector<int> range(int l,int r) {
-        vector<int> res;
-        for(l += length, r += length; l < r; l >>=1, r >>=1) {
-            if(l&1) res.push_back(l++);
-            if(r&1) res.push_back(--r);
-        }
-        return res;
-    }
-    inline int operator[](int idx) {
-		return idx+length;
-	}
-    inline size_t size(void){
-        return length;
     }
 };
 
@@ -73,8 +92,8 @@ int main() {
     for(int i = 0; i < N; ++i) cin >> X[i];
     X.push_back(1e10);
     RangeIndex ri(N);
-    UnionFindTree uf(2*ri.size());
-    vector<int> st(2*ri.size(),0);
+    UnionFindTree uf(ri.size());
+    vector<int> st(ri.size(),0);
     for(int i = 0; i < N; ++i) {
         int l = lower_bound(X.begin(),X.end(),X[i]+A)-X.begin();
         int r = upper_bound(X.begin(),X.end(),X[i]+B)-X.begin();
@@ -84,10 +103,9 @@ int main() {
         }
     }
     for(int i = 0; i < N; ++i) {
-        int j=i+ri.size();
-        while(j >>= 1) if(st[j]) uf.merge(ri[i],j);
+        for(auto& e:ri.include_range(i)) if(st[e]) uf.merge(ri[i],e);
     }
-    vector<int> cnt(2*ri.size(),0);
+    vector<int> cnt(ri.size(),0);
     for(int i = 0; i < N; ++i) {
         cnt[uf[ri[i]]]++;
     }
